b897b1b850deb5b59e9681fdd849ab0011801095
sam <samuel.brb19@gmail.com>
2021-04-10 14:56:37 +0200

    GameManager + Bullet

---
+++ Assets/BulletBehavior1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BulletBehavior1 : MonoBehaviour,IPooledObject
+{
+    private GameManager _gm;
+    public float bulletSpeed;
+    private Vector3 _direction;
+    private void Start()
+    {
+        _gm = GameManager.Instance;
+    }
+
+    public void OnObjectSpawn(Vector3 direction)
+    {
+        _direction = direction;
+    }
+
+    private void Update()
+    {
+        transform.Translate(_direction * Time.deltaTime* bulletSpeed);
+        if (transform.position.x > _gm.screenBounds.x || transform.position.x < -_gm.screenBounds.x || transform.position.y > _gm.screenBounds.y || transform.position.y < -_gm.screenBounds.y)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
